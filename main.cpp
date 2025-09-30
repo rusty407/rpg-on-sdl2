f3280d4c8350c0570e6ad283e70dafe63829e5f7
rusty407 <sandroamiridze93@gmail.com>
2025-09-30 18:04:56 -0400

    added debugging while creating windows in init()

--- main.cpp
+++ main.cpp
@@ -3,7 +3,28 @@
 #include <cstddef>
 #include <stdio.h>
 
-const SCREEN_WIDTH
+const int SCREEN_WIDTH = 1024;
+const int SCREEN_HEIGHT = 768;
+
+SDL_Window* gWindow = NULL;
+SDL_Surface* gScreenSurface = NULL;
+SDL_Surface* gBackground = NULL;
+
+bool init() {
+   bool success = true; 
+   if(SDL_Init(SDL_INIT_VIDEO < 0) {
+        printf("Failed to execute window %d\n", SDL_GetError());
+        success = false;
+   }
+}
+bool loadMedia();
+void close();
 
 int main( int argc, char* args[] ) {
+    if(!init()) {
+        printf("Failure during executing program");
+    } else if (!loadMedia()) {
+        printf("Failure during executing media files");
+    }
+
 }
