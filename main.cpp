8d8cedf2a6df759b485b90b8fb3c4912efdd5555
rusty407 <sandroamiridze93@gmail.com>
2025-09-30 18:26:02 -0400

    creation of window but better with debugging

--- main.cpp
+++ main.cpp
@@ -10,18 +10,27 @@ SDL_Window* gWindow = NULL;
 SDL_Surface* gScreenSurface = NULL;
 SDL_Surface* gBackground = NULL;
 
+
+// with this function we initialize creation of window in SDL
 bool init() {
    bool success = true; 
-   if(SDL_Init(SDL_INIT_VIDEO < 0) {
-        printf("Failed to execute window %d\n", SDL_GetError());
+   if(SDL_Init(SDL_INIT_VIDEO) < 0) {
+        printf("Failed to execute window %s\n", SDL_GetError());
         success = false;
    } else {
-       gWindow = SDL_CreateWindow("rpg_game", SDL_POSITION_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+       gWindow = SDL_CreateWindow("rpg_game", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+       if (gWindow == NULL) {
+            printf("Window creation was failed because of: %s\n", SDL_GetError());
+            success = false;
+       } else {
+            gScreenSurface = SDL_GetWindowSurface(gWindow);
+       }
    }
+   return success; // return success cuz we got boolean value over there so function needs to know return is true of false to execute
 }
 
-bool loadMedia();
-void close();
+bool loadMedia(); 
+//void close();
 
 int main( int argc, char* args[] ) {
     if(!init()) {
