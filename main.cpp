21e774c8e2be3ad39dec400d2896afe6c2a06bc0
rusty407 <sandroamiridze93@gmail.com>
2025-10-01 23:01:01 -0400

    added enums for controls and added loadSurface function for different images

--- main.cpp
+++ main.cpp
@@ -1,14 +1,26 @@
 #include <SDL.h>
 #include <cstddef>
 #include <stdio.h>
+#include <string>
 
 const int SCREEN_WIDTH = 1024;
 const int SCREEN_HEIGHT = 768;
 
+enum KeySurfacePresses {
+    KEY_SURFACE_PRESS_DEFAULT,
+    KEY_SURFACE_PRESS_LEFT,
+    KEY_SURFACE_PRESS_RIGHT,
+    KEY_SURFACE_PRESS_DOWN,
+    KEY_SURFACE_PRESS_UP,
+    KEY_SURFACE_PRESS_TOTAL
+}
+
 SDL_Window* gWindow = NULL;
 SDL_Surface* gScreenSurface = NULL;
 SDL_Surface* gBackground = NULL;
-
+SDL_Surface* gKeySurfacePresses[ KEY_SURFACE_PRESS_TOTAL ];
+SDL_Surface* gCurentSurface = NULL;
+ 
 // with this function we initialize creation of window in SDL
 bool init() {
    bool success = true; 
@@ -16,7 +28,7 @@ bool init() {
         printf("Failed to execute window %s\n", SDL_GetError());
         success = false;
    } else {
-       gWindow = SDL_CreateWindow("rpg_game", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+       gWindow = SDL_CreateWindow("rpg_game", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN); // title, default pos, def pos, swidth, sheight, window_shown
        if (gWindow == NULL) {
             printf("Window creation was failed because of: %s\n", SDL_GetError());
             success = false;
@@ -37,6 +49,14 @@ bool loadMedia() {
     } 
     return success;
 }; 
+
+SDL_Surface* loadSurface( std::string path ) {
+    SDL_Surface* loadedSurface = SDL_LoadBMP( path.c_str() );
+    if(loadedSurface == NULL) {
+        printf("failure during loading surface and rendering image: %s\n", path.c_str(),  SDL_GetError());
+    } 
+    return loadedSurface;
+}
 
 void close() {
     // delocate surface
@@ -59,7 +79,7 @@ int main( int argc, char* args[] ) {
         // don't close before i click the close button
         SDL_Event e;
         bool quit = false;
-        while(quit == false) {
+        while(!quit) {
             while(SDL_PollEvent(&e)) {
                 if(e.type == SDL_QUIT) {
                     quit = true;
